fa2993d0300f238eab033c465e8ff9e6dfe56311
RGV <ricardo.guzman.velasco@outlook.com>
2021-05-15 16:09:06 +0200

    1 | Dollar multiplication | Green

--- Runtime/Dollar.cs
+++ Runtime/Dollar.cs
@@ -2,7 +2,7 @@ namespace Domain
 {
     public class Dollar
     {
-        public int Amount { get; }
+        public int Amount { get; } = 10;
         
         public Dollar(int amount)
         {
