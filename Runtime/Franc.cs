8ee805b1d46099b42130a06082232084ff59ead5
RGV <ricardo.guzman.velasco@outlook.com>
2021-05-15 16:47:17 +0200

    5 | Franc multiplication | Red

---
+++ Runtime/Franc.cs
@@ -0,0 +1,15 @@
+namespace Domain
+{
+    public class Franc
+    {
+        public Franc(int amount)
+        {
+            
+        }
+
+        public Franc Times(int multiplier)
+        {
+            return null;
+        }
+    }
+}
--- Tests/CurrencyTests.cs
+++ Tests/CurrencyTests.cs
@@ -6,6 +6,18 @@ namespace Domain.Tests
     public class CurrencyTests
     {
         [Test]
+        public void Franc_Multiplication()
+        {
+            var sut = new Franc(5);
+
+            var result1 = sut.Times(2);
+            var result2 = sut.Times(3);
+            
+            result1.Should().Be(new Franc(10));
+            result2.Should().Be(new Franc(15));
+        }
+        
+        [Test]
         public void Dollar_Multiplication()
         {
             var sut = new Dollar(5);
