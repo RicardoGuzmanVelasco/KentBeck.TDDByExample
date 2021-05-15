8cb10a078bc8ed4d1b8f5afa0ab179ce9dbf9ac5
RGV <ricardo.guzman.velasco@outlook.com>
2021-05-15 17:16:43 +0200

    7 | Dollar-Franc inequality | Green

--- Runtime/Money.cs
+++ Runtime/Money.cs
@@ -5,6 +5,11 @@ namespace Domain
         protected readonly int amount;
 
         protected Money(int amount) => this.amount = amount;
-        public override bool Equals(object o) => o is Money other && amount == other.amount;
+        public override bool Equals(object o)
+        {
+            return o is Money other &&
+                   GetType() == other.GetType() &&
+                   amount == other.amount;
+        }
     }
 }
