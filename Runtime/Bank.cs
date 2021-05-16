53f2961354a873ab5cdc094658728c9f97c636a1
RGV <ricardo.guzman.velasco@outlook.com>
2021-05-16 11:37:53 +0200

    18 | Bank rates | Green

--- Runtime/Bank.cs
+++ Runtime/Bank.cs
@@ -4,15 +4,20 @@ namespace Domain
 {
     public class Bank
     {
-        Dictionary<CurrencyPair, int> rates;
+        readonly Dictionary<CurrencyPair, int> rates = new Dictionary<CurrencyPair, int>();
         
         public Money Reduce(MoneyExpression source, string to) => source.Reduce(this, to);
 
         public void AddRate(string from, string to, int rate)
         {
-            
+            rates[new CurrencyPair(from, to)] = rate;
         }
 
-        public int Rate(string from, string to) => rates[new CurrencyPair(@from, to)];
+        public int Rate(string from, string to)
+        {
+            return @from == to
+                    ? 1
+                    : rates[new CurrencyPair(@from, to)];
+        }
     }
 }
